5e05af1c9a20032892649e7ea4f961cf47135f77
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-03-03 12:22:44 +0100

    task

--- components/Contact.js
+++ components/Contact.js
@@ -8,7 +8,7 @@ var Contact = React.createClass({
         <div className={'contactItem'}>
             <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
             <p className={'contactLabel'}>
-            Name: {this.props.contact.firstName}
+            Name: {this.props.item.firstName}
             </p>
             <a href={'mailto:' + this.props.item.email}>
             {this.props.item.email}
